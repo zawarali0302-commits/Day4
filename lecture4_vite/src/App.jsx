8d6f5b10cd7405b8a037f7328c06217ba12255ee
zawarali0302-commits <zawarali0302@gmail.com>
2025-09-13 02:24:30 -0700

    Initial commit

---
+++ lecture4_vite/src/App.jsx
@@ -0,0 +1,84 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="font-bold font-serif text-emerald-500">Flags</h1><br />
+      <div className="flex flex-row gap-2">
+        <div className="flex w-72 h-52 border">
+          <div className="bg-blue-500 w-1/3 "></div>
+          <div className="bg-white w-1/3 "></div>
+          <div className="bg-red-500 w-2/3 "></div>
+          <div className="bg-white w-1/3  "></div>
+          <div className="bg-blue-500 w-1/3 "></div>
+        </div>
+
+
+        <div className="flex flex-col w-72 h-52 border">
+          <div className="bg-blue-500 h-1/3 "></div>
+          <div className="bg-white h-1/3 "></div>
+          <div className="bg-red-500 h-2/3 "></div>
+          <div className="bg-white h-1/3  "></div>
+          <div className="bg-blue-500 h-1/3 "></div>
+        </div>
+
+
+        <div className="flex bg-white w-72 h-52 border items-center justify-center">
+          <div className="bg-red-500 size-30 rounded-full "></div>
+        </div>
+
+        <div className="flex flex-col w-72 h-52 border">
+          <div className="bg-blue-500 h-1/3 "></div>
+          <div className="bg-white h-1/3 "></div>
+          <div className="bg-red-500 h-2/3 flex items-center justify-center">
+            <div className="bg-green-500 size-20 rounded-full"></div>
+          </div>
+          <div className="bg-white h-1/3  "></div>
+          <div className="bg-blue-500 h-1/3 "></div>
+        </div>
+      </div>
+
+
+      <div className="flex flex-col gap-2 mt-2">
+        <div className="flex w-72 h-52 border">
+          <div className="bg-blue-500 w-1/3 "></div>
+          <div className="bg-white w-1/3 "></div>
+          <div className="bg-red-500 w-2/3 "></div>
+          <div className="bg-white w-1/3  "></div>
+          <div className="bg-blue-500 w-1/3 "></div>
+        </div>
+
+        <div className="flex flex-col w-72 h-52 border">
+          <div className="bg-blue-500 h-1/3 "></div>
+          <div className="bg-white h-1/3 "></div>
+          <div className="bg-red-500 h-2/3 "></div>
+          <div className="bg-white h-1/3  "></div>
+          <div className="bg-blue-500 h-1/3 "></div>
+        </div>
+
+        <div className="flex bg-white w-72 h-52 border items-center justify-center">
+          <div className="bg-red-500 size-30 rounded-full "></div>
+        </div>
+        <div className="flex flex-col w-72 h-52 border">
+          <div className="bg-blue-500 h-1/3 "></div>
+          <div className="bg-white h-1/3 "></div>
+          <div className="bg-red-500 h-2/3 flex items-center justify-center">
+            <div className="bg-green-500 size-20 rounded-full"></div>
+          </div>
+          <div className="bg-white h-1/3  "></div>
+          <div className="bg-blue-500 h-1/3 "></div>
+        </div>
+      </div>
+
+      <h1>Bangladesh Flag</h1>
+      <div className="bg-[rgb(0,103,69)] border w-72 h-52 flex items-center justify-center">
+        <div className="bg-[rgb(219,36,32)] size-25 rounded-full -ml-10"></div>
+        
+      </div>
+    </>
+
+  )
+}
+
+export default App
